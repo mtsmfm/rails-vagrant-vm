6e02a3a5d4acd05e51c0589a09ffaaa4573cac83
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-11-16 21:15:17 +0900

    metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'rails-vagrant-vm'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'mtsmfm'
+maintainer_email 'mtsmfm@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures rails-vagrant-vm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
