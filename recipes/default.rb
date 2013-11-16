31e41f419688e949dd620d1bb33aecf7b163f26b
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-11-17 02:28:56 +0900

    Recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,19 @@
 # Cookbook Name:: rails-vagrant-vm
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
+# Copyright (C) 2013 mtsmfm
+#
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'git'
+include_recipe 'heroku'
+include_recipe 'postgresql'
+include_recipe 'postgresql::client'
+include_recipe 'tmux'
+include_recipe 'vim'
+include_recipe 'zsh'
+include_recipe 'rbenv'
+include_recipe 'rbenv::ruby_build'
+include_recipe 'rbenv::rbenv_vars'
+rbenv_ruby '2.1.0-preview1'
