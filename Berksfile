597e609abc3b4517562417b5ca0766a71c4d5e00
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-11-17 02:26:43 +0900

    Add dependencies

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 site :opscode
 
 metadata
+
+cookbook 'rbenv', github: 'schisamo/rbenv-cookbook', branch: 'CHEF-3940'
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,10 @@ description      'Installs/Configures rails-vagrant-vm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends          'git'
+depends          'heroku'
+depends          'postgresql'
+depends          'rbenv'
+depends          'tmux'
+depends          'vim'
+depends          'zsh'
